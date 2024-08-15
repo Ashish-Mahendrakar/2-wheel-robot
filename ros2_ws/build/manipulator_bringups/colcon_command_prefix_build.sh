f6b644e207d9245824aeb83df9478d5411b05399
Ashish-Mahendrakar <ashish.mahendrakar2268@gmail.com>
2024-08-16 00:16:22 +0530

    ros2_ws/

---
+++ ros2_ws/build/manipulator_bringups/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ashish/ros2_ws/install/manipulator_description/share/manipulator_description/package.sh"
---
+++ ros2_ws/build/manipulator_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashish/ros2_ws/src/manipulator_description
+# Build directory: /home/ashish/ros2_ws/build/manipulator_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
